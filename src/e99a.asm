9d52743f2ea5295057fb9bce2e697a3a1524c69a
lyre <lyre@poetpalace.org>
2021-08-28 21:36:51 +0800

    the intr works on dosbos-x on mac

--- src/e99a.asm
+++ src/e99a.asm
@@ -4,9 +4,9 @@ start:
         mov ax,0b800H
         mov ds,ax
         
-        mov ds:[9a0h], 0230h ;  a0h for 160, the second line, since the first line get wiped out,  0230h: 30 for '0', 02 for attribute
-        mov ds:[9a2h], 2430h
-        mov ds:[9a4h], 7130h
+        mov ds:[160 * 1 + 0], 0230h ;  160 * 1 for line 1,  0230h: 30 for '0', 02 for attribute
+        mov ds:[160 * 1 + 2], 2430h
+        mov ds:[160 * 1 + 4], 7130h
 
         mov ax,4c00h
         int 21h
--- src/intr.asm
+++ src/intr.asm
@@ -44,32 +44,25 @@ do0:
     db "overflow!"      ; 0000:0202h
 
 do0start:
-    mov ax,0b800H
-    mov ds,ax
-    
-    mov ds:[1a0h], 0230h ;  a0h for 160, the second line, since the first line get wiped out,  0230h: 30 for '0', 02 for attribute
-    mov ds:[1a2h], 2430h
-    mov ds:[1a4h], 7130h
+    ; ds:si -> src stri
+    mov ax, cs
+    mov ds, ax
+    mov si, 202h   ; hmm?
 
-;    ; ds:si -> src stri
-;    mov ax, cs
-;    mov ds, ax
-;    mov si, 202h   ; hmm?
-;
-;    ; es:di -> middle of the screenng
-;    mov ax, 0b800h
-;    mov es, ax
-;    mov di, 12*160 + 36*2   
-;
-;    mov cx, 9 ; string length
-;s:
-;    mov al,[si]
-;    mov es:[di],al
-;    inc di
-;    mov byte ptr es:[di], 24h;
-;    inc di
-;    inc si
-;    loop s
+    ; es:di -> middle of the screenng
+    mov ax, 0b800h
+    mov es, ax
+    mov di, 12*160 + 36*2   
+
+    mov cx, 9 ; string length
+s:
+    mov al,[si]
+    mov es:[di],al
+    inc di
+    mov byte ptr es:[di], 24h;   // Forground Red, Background Green
+    inc di
+    inc si
+    loop s
 
     mov ax, 4c00h
     int 21h
